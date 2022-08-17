4cc0cb2121fbbc1de9f82f649b1c6c0f4189f636
KevinCarm <kevincarm016@hotmail.com>
2022-08-16 21:53:11 -0500

    cCreate user functionality added in user controller

--- controllers/usercontroller.go
+++ controllers/usercontroller.go
@@ -1,1 +1,30 @@
 package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"go-api-jwt/database"
+	"go-api-jwt/models"
+)
+
+func Insert(c *fiber.Ctx) error {
+	var user models.User
+
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"code":    fiber.StatusBadRequest,
+			"message": "Bad request",
+			"error":   err.Error(),
+		})
+	}
+
+	record := database.Instance.Create(&user)
+	if record.Error != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code":    fiber.StatusInternalServerError,
+			"message": "Internal error",
+			"error":   record.Error.Error(),
+		})
+	}
+
+	return c.JSON(user)
+}
