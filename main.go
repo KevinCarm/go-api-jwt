5c04e9d13eb8b54d5749b31c8067af39f517e252
KevinCarm <kevincarm016@hotmail.com>
2022-08-17 21:29:29 -0500

    Product crud created

--- main.go
+++ main.go
@@ -12,10 +12,18 @@ func main() {
 	database.Migrate()
 
 	app := fiber.New()
+	//User routes
 	app.Post("/api/users", controllers.CreateUser)
 	app.Get("/api/users", controllers.GetAllUsers)
 	app.Get("/api/users/:id", controllers.GetUserById)
 	app.Delete("/api/users/:id", controllers.DeleteUserById)
 	app.Put("/api/users/:id", controllers.UpdateUser)
+	//Product routes
+	app.Post("/api/products", controllers.CreateProduct)
+	app.Get("/api/products", controllers.GetAllProducts)
+	app.Get("/api/products/:id", controllers.GetProductById)
+	app.Delete("/api/products/:id", controllers.DeleteProduct)
+	app.Put("/api/products/:id", controllers.UpdateProduct)
+	log.Println("Running server on port 8080")
 	log.Fatal(app.Listen(":8080"))
 }
